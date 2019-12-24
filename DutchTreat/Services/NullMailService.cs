cc461b411f2e242bfa7c227d35ec4dda492e48d8
James Hart <james.elliot.hart@gmail.com>
2019-12-23 19:47:39 -0600

    add and register mail service

---
+++ DutchTreat/Services/NullMailService.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.Logging;
+
+namespace DutchTreat.Services
+{
+    public class NullMailService : IMailService
+    {
+        private readonly ILogger<NullMailService> logger;
+
+        public NullMailService(ILogger<NullMailService> logger)
+        {
+            this.logger = logger;
+        }
+        public void SendMessage(string to, string subject, string body)
+        {
+            logger.LogInformation($"To: {to} Subject: {subject} Body: {body}");
+        }
+    }
+}
