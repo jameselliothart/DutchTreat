051c17d2e8ae86ab4b3c735b4a0575f6ce46fcdb
James Hart <james.elliot.hart@gmail.com>
2019-12-30 19:16:17 -0600

    add some logging to the repository

--- DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/Data/DutchRepository.cs
@@ -1,23 +1,35 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
 
 namespace DutchTreat.Data
 {
     public class DutchRepository : IDutchRepository
     {
         private readonly DutchContext ctx;
+        private readonly ILogger<DutchRepository> logger;
 
-        public DutchRepository(DutchContext ctx)
+        public DutchRepository(DutchContext ctx, ILogger<DutchRepository> logger)
         {
             this.ctx = ctx;
+            this.logger = logger;
         }
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return ctx.Products
-                        .OrderBy(p => p.Title)
-                        .ToList();
+            try
+            {
+                return ctx.Products
+                            .OrderBy(p => p.Title)
+                            .ToList();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Failed to get all products: {ex}");
+                return null;
+            }
         }
 
         public IEnumerable<Product> GetProductByCategory(string category)
