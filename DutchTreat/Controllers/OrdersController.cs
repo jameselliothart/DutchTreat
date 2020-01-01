3086b3743e13d71f8e22290e52d3094c4969ac17
James Hart <james.elliot.hart@gmail.com>
2019-12-31 19:24:19 -0600

    use order view model in post

--- DutchTreat/Controllers/OrdersController.cs
+++ DutchTreat/Controllers/OrdersController.cs
@@ -1,6 +1,7 @@
 using System;
 using DutchTreat.Data;
 using DutchTreat.Data.Entities;
+using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -52,16 +53,41 @@ namespace DutchTreat.Controllers
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody] Order model)
+        public IActionResult Post([FromBody] OrderViewModel model)
         {
             var msg = "Failed to save a new order";
             
             try
             {
-                repository.AddEntity(model);
-                if (repository.SaveAll())
+                if (ModelState.IsValid)
                 {
-                    return CreatedAtAction(nameof(Get), new { id = model.Id}, model);
+                    var newOrder = new Order()
+                    {
+                        Id          = model.OrderId,
+                        OrderDate   = model.OrderDate,
+                        OrderNumber = model.OrderNumber,
+                    };
+
+                    if (newOrder.OrderDate == DateTime.MinValue)
+                    {
+                        newOrder.OrderDate = DateTime.Now;
+                    }
+
+                    repository.AddEntity(newOrder);
+                    if (repository.SaveAll())
+                    {
+                        var vm = new OrderViewModel()
+                        {
+                            OrderId     = newOrder.Id,
+                            OrderDate   = newOrder.OrderDate,
+                            OrderNumber = newOrder.OrderNumber,
+                        };
+                        return CreatedAtAction(nameof(Get), new { id = vm.OrderId}, vm);
+                    }
+                }
+                else
+                {
+                    return BadRequest(ModelState);
                 }
             }
             catch (Exception ex)
