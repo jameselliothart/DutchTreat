632b5d5f3ceda54674720a84f61a0ac213e58bc0
James Hart <james.elliot.hart@gmail.com>
2019-12-31 19:50:19 -0600

    create order item view model

---
+++ DutchTreat/ViewModels/OrderItemViewModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DutchTreat.ViewModels
+{
+    public class OrderItemViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public decimal UnitPrice { get; set; }
+    }
+}
