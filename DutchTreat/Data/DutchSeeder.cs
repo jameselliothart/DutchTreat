2afae605a402b3a83b4adba85f616804bdf8f2f8
James Hart <james.elliot.hart@gmail.com>
2020-01-01 19:41:15 -0600

    update seeder for store user identity

--- DutchTreat/Data/DutchSeeder.cs
+++ DutchTreat/Data/DutchSeeder.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
+using System.Threading.Tasks;
 using DutchTreat.Data.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 
 namespace DutchTreat.Data
 {
@@ -11,16 +14,40 @@ namespace DutchTreat.Data
     {
         private readonly DutchContext ctx;
         private readonly IWebHostEnvironment hosting;
+        private readonly UserManager<StoreUser> userManager;
 
-        public DutchSeeder(DutchContext ctx, IWebHostEnvironment hosting)
+        public DutchSeeder(
+            DutchContext ctx, 
+            IWebHostEnvironment hosting,
+            UserManager<StoreUser> userManager
+            )
         {
             this.ctx = ctx;
             this.hosting = hosting;
+            this.userManager = userManager;
         }
 
-        public void Seed()
+        public async Task SeedAsync()
         {
             ctx.Database.EnsureCreated();
+
+            StoreUser user = await userManager.FindByEmailAsync("james@dutchtreat.com");
+            if (user == null)
+            {
+                user = new StoreUser()
+                {
+                    FirstName = "James",
+                    LastName  = "Hart",
+                    Email     = "james@dutchtreat.com",
+                    UserName  = "james@dutchtreat.com",
+                };
+
+                var result = await userManager.CreateAsync(user, "P@ssw0rd!");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create new user in seeder");
+                }
+            }
 
             if (!ctx.Products.Any())
             {
@@ -32,6 +59,7 @@ namespace DutchTreat.Data
                 var order = ctx.Orders.Where(o => o.Id == 1).FirstOrDefault();
                 if (order != null)
                 {
+                    order.User = user;
                     order.Items = new List<OrderItem>()
                     {
                         new OrderItem()
